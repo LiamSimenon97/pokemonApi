2329f9f1285ea8f9cfabc0e6ec05bc54b435ce60
Liam Simenon <lsi@nexiosit.com>
2024-01-24 20:01:49 +0100

    Project init

---
+++ pokemon-api/src/main.ts
@@ -0,0 +1,8 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(3000); // This is the port number that the server will listen on
+}
+bootstrap();
